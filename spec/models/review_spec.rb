3b886d2371a0c5ee23995f89240a6500964373c9
Chris Parker <cjp214@gmail.com>
2016-04-12 15:58:05 +0100

    added validation for restaurant uniqueness and max rating

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,6 +4,13 @@ require 'rails_helper'
 #   pending "add some examples to (or delete) #{__FILE__}"
 # end
 
-describe Review, type: :model do
+describe Review, :type => :model do
   it { should belong_to :restaurant }
+
+  it 'is invalid if the rating is more than 5' do
+    review = Review.new(rating: 10)
+    expect(review).to have(1).error_on(:rating)
+  end
+
+
 end
