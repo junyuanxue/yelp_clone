177794f591196425d9e210a49f889bb93710658f
Chris Parker <cjp214@gmail.com>
2016-04-12 15:38:27 +0100

    adding validation matchers for restaurant names

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'capybara'
   gem 'shoulda'
+  gem 'rspec-collection_matchers'
 end
 
 group :development do
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
+require 'spec_helper'
 
-describe Restaurant, type: :model do
+describe Restaurant, :type => :model do
   it { should have_many :reviews }
+
+  it 'is not valid with a name of less than three characters' do
+    restaurant = Restaurant.new(name: "kf")
+    expect(restaurant).to have(1).error_on(:name)
+    expect(restaurant).not_to be_valid
+  end
+
 end
