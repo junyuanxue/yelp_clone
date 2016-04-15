5cec4d7653af7c1e1ab468099357f032079ea18f
Junyuan Xue <junyuanxue.is@gmail.com>
2016-04-15 11:05:35 +0100

    all tests passing

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,7 +7,7 @@ class ReviewsController < ApplicationController
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = @restaurant.build_review review_params, current_user
+    @review = @restaurant.reviews.build_with_user review_params, current_user
 
     if @review.save
       redirect_to restaurants_path
@@ -17,9 +17,9 @@ class ReviewsController < ApplicationController
         redirect_to restaurants_path
       else
         render :new
+      end
     end
   end
-end
 
   def review_params
     params.require(:review).permit(:thoughts, :rating)
